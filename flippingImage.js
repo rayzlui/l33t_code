105ccdca0118d27750d767c6782dcf43cd0d63cc
Ray <rayzlui@gmail.com>
2019-07-24 21:24:37 -0700

    flippingImage for leetcode

---
+++ flippingImage.js
@@ -0,0 +1,14 @@
+//From 832 of LeetCode
+
+/**
+ * @param {number[][]} A
+ * @return {number[][]}
+ */
+
+var flipAndInvertImage = function(array) {
+  return array.reduce((result, current) => {
+      result.push(current.reverse().map(x => x == 0 ? 1 : 0))
+      return result
+  },[])
+  
+};
