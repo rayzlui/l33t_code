77ec6493bcfea31043825032303646e11f7ba26a
Ray <rayzlui@gmail.com>
2019-07-24 21:26:22 -0700

    judgeCirle for leetcode

---
+++ judgeCircle.js
@@ -0,0 +1,27 @@
+//From 657 in LeetCode
+
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function(moves) {
+    let vertical = 0
+    let horizontal = 0
+    for(x of moves) {
+        switch(x){
+            case "L":
+                vertical --
+                break;
+            case "R":
+                vertical ++
+                break;
+            case "U":
+                horizontal ++
+                break;
+            case "D":
+                horizontal --
+                break;
+        }
+    }
+    return (vertical === 0 ) && (horizontal === 0)
+};
