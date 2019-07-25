44f91d588de6e55723e6d565e3d26d29f300a45b
Ray <rayzlui@gmail.com>
2019-07-24 21:25:14 -0700

    morseCode for leetcode

---
+++ morseCode.js
@@ -0,0 +1,48 @@
+//From 804 in LeetCode
+
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+var uniqueMorseRepresentations = function(words) {
+    const morseCode = { 
+        a:".-", 
+        b:"-...",
+        c:"-.-.",
+        d:"-..",
+        e:".",
+        f:"..-.",
+        g:"--.",
+        h:"....",
+        i:"..",
+        j:".---",
+        k: "-.-",
+        l:".-..", 
+        m:"--", 
+        n :"-.", 
+        o:"---", 
+        p: ".--.", 
+        q:"--.-", 
+        r:".-.", 
+        s:"...", 
+        t:"-", 
+        u: "..-", 
+        v:"...-", 
+        w: ".--", 
+        x: "-..-", 
+        y:"-.--",
+        z:"--..",
+    }
+    function checkWord(acc, current) {
+        let code = ""
+        for (let a of current){
+            code += morseCode[a]
+        }
+        if (!acc.includes(code)){
+            acc.push(code)
+        }
+        return acc
+    }
+    
+    return words.reduce(checkWord, []).length
+};
