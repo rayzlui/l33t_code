fb26a69d93e758ceb695580712227a6ec7118407
Ray <rayzlui@gmail.com>
2019-07-30 22:16:40 -0700

    521 from leetcode

---
+++ longestUncommonSubS.js
@@ -0,0 +1,11 @@
+//From 521 in LeetCode
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+function findLUSlength(a, b) {
+  if (a === b) return -1;
+  return a.length > b.length ? a.length : b.length;
+}
