e5ba52ff149556685d9ad7157f6ad90b5f9b98e2
Ray <rayzlui@gmail.com>
2019-08-20 21:44:09 -0700

    122 from leetcode

---
+++ maxProfit.js
@@ -0,0 +1,15 @@
+//From 122 in LeetCode
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+function maxProfit(prices) {
+  let maxProfit = 0;
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      maxProfit += prices[i] - prices[i - 1];
+    }
+  }
+  return maxProfit;
+}
