54547e48f99353dbb2e2e079ab960b1f85b9453f
Ray <rayzlui@gmail.com>
2019-10-17 22:35:27 -0700

    1 from leetcode

---
+++ sumIndices.js
@@ -0,0 +1,16 @@
+//From 1 in LeetCode
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+}
