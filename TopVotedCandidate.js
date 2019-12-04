99cf0435a2db370cfba5fd4a0c67096b126bb533
Ray <rayzlui@gmail.com>
2019-12-03 23:13:49 -0800

    911 from leetcode

---
+++ TopVotedCandidate.js
@@ -0,0 +1,43 @@
+//From 911 in LeetCode
+/**
+ * @param {number[]} persons
+ * @param {number[]} times
+ */
+class TopVotedCandidate {
+  constructor(persons, times) {
+    this.votes = {};
+    this.tally = {};
+    let leader = persons[0];
+    for (let i = 0; i < persons.length; i++) {
+      let vote = persons[i];
+      let time = times[i];
+      if (this.tally[vote]) {
+        this.tally[vote]++;
+      } else {
+        this.tally[vote] = 1;
+      }
+      if (this.tally[vote] >= this.tally[leader]) {
+        this.votes[time] = vote;
+        leader = vote;
+      }
+    }
+  }
+}
+
+/**
+ * @param {number} t
+ * @return {number}
+ */
+TopVotedCandidate.prototype.q = function(t) {
+  for (let i = t; i >= 0; i--) {
+    if (this.votes[i] !== undefined) {
+      return this.votes[i];
+    }
+  }
+};
+
+/**
+ * Your TopVotedCandidate object will be instantiated and called as such:
+ * var obj = new TopVotedCandidate(persons, times)
+ * var param_1 = obj.q(t)
+ */
