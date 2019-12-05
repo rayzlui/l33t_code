5b98e74fb9165112126f2aba747f50661f3aa654
Ray <rayzlui@gmail.com>
2019-12-04 23:00:48 -0800

    update for speed

--- TopVotedCandidate.js
+++ TopVotedCandidate.js
@@ -41,3 +41,49 @@ TopVotedCandidate.prototype.q = function(t) {
  * var obj = new TopVotedCandidate(persons, times)
  * var param_1 = obj.q(t)
  */
+
+//faster
+
+/**
+ * @param {number[]} persons
+ * @param {number[]} times
+ */
+class TopVotedCandidateII {
+  constructor(persons, times) {
+    this.votes = {};
+    this.tally = {};
+    let leader = persons[0];
+    for (let i = 0; i < persons.length; i++) {
+      let vote = persons[i];
+      let time = times[i];
+      if (this.tally[vote]) {
+        this.tally[vote]++;
+      } else {
+        this.tally[vote] = 1;
+      }
+      if (this.tally[vote] >= this.tally[leader]) {
+        this.votes[time] = vote;
+        leader = vote;
+      } else {
+        this.votes[time] = leader;
+      }
+    }
+
+    this.time = times.reverse();
+  }
+}
+
+/**
+ * @param {number} t
+ * @return {number}
+ */
+TopVotedCandidate.prototype.q = function(t) {
+  let index = this.time.find(x => x <= t);
+  return this.votes[index];
+};
+
+/**
+ * Your TopVotedCandidate object will be instantiated and called as such:
+ * var obj = new TopVotedCandidate(persons, times)
+ * var param_1 = obj.q(t)
+ */
