0bedd67be35490fa17ecbd13658c50ec9ca3afce
Ray <rayzlui@gmail.com>
2019-12-24 22:10:21 -0800

    faster code

--- threeSumClosest.js
+++ threeSumClosest.js
@@ -25,4 +25,30 @@ function threeSumClosest(nums, target) {
   return holder[keys[0]];
 }
 
+//faster
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+function threeSumClosestII(nums, target) {
+  let holder = {};
+  for (let i = 0; i < nums.length - 2; i++) {
+    for (let j = i + 1; j < nums.length - 1; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        let x = nums[i];
+        let y = nums[j];
+        let z = nums[k];
+        let totalDiff = Math.abs(x + y + z - target);
+        if (totalDiff === 0) {
+          return x + y + z;
+        }
+        holder[totalDiff] = x + y + z;
+      }
+    }
+  }
+
+  let keys = Object.keys(holder);
+  return holder[keys[0]];
+}
