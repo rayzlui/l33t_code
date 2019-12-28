591de18a160f65c3b0ea6cf95ab318730d4ab9d4
Ray <rayzlui@gmail.com>
2019-12-27 22:45:21 -0800

    380 from leetcode

---
+++ RandomizedSet.js
@@ -0,0 +1,55 @@
+//From 380 in LeetCode
+
+/**
+ * Initialize your data structure here.
+ */
+class RandomizedSet {
+  constructor() {
+    this.set = {};
+  }
+}
+
+/**
+ * Inserts a value to the set. Returns true if the set did not already contain the specified element.
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+  if (this.set[val]) {
+    return false;
+  } else {
+    this.set[val] = true;
+    return true;
+  }
+};
+
+/**
+ * Removes a value from the set. Returns true if the set contained the specified element.
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+  if (this.set[val]) {
+    delete this.set[val];
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/**
+ * Get a random element from the set.
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+  let keys = Object.keys(this.set);
+  return keys[Math.floor(Math.random() * keys.length)];
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
