6269597d346a80800acb94e32bfb4d1798988ace
Ray <rayzlui@gmail.com>
2020-01-08 22:13:33 -0800

    435 from leetcode

---
+++ eraseOverlap.js
@@ -0,0 +1,20 @@
+//From 435 in LeetCode
+
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+function eraseOverlapIntervals(intervals) {
+  let count = 0;
+  intervals = intervals.sort((x, y) => x[1] - y[1]);
+  for (let i = 1; i < intervals.length; i++) {
+    let current = intervals[i];
+    let previous = intervals[i - 1];
+    if (current[0] < previous[1]) {
+      count++;
+      intervals.splice(i, 1);
+      i--;
+    }
+  }
+  return count;
+}
