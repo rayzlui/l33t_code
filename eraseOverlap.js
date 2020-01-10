c726c18960db9e3334088fba2ace428eddc90af5
Ray <rayzlui@gmail.com>
2020-01-09 21:58:30 -0800

    435 from leetcode

--- eraseOverlap.js
+++ eraseOverlap.js
@@ -18,3 +18,24 @@ function eraseOverlapIntervals(intervals) {
   }
   return count;
 }
+
+//faster
+
+/**
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+function eraseOverlapIntervalsII(intervals) {
+  let count = 0;
+  intervals = intervals.sort((x, y) => x[1] - y[1]);
+  let previous = intervals[0];
+  for (let i = 1; i < intervals.length; i++) {
+    let current = intervals[i];
+    if (current[0] < previous[1]) {
+      count++;
+    } else {
+      previous = current;
+    }
+  }
+  return count;
+}
