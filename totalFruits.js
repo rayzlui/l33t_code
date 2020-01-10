44c0931c9f5aa06caa355f85d30f79e2e52d5741
Ray <rayzlui@gmail.com>
2020-01-09 21:58:50 -0800

    904 from leetcode

---
+++ totalFruits.js
@@ -0,0 +1,44 @@
+//From 904 in LeetCode
+
+/**
+ * @param {number[]} tree
+ * @return {number}
+ */
+function totalFruit(tree) {
+  //want greatest subarray with two elements
+  let result = 0;
+  let frontNum = null;
+  let lastFrontIndex = null;
+  let secondNum = null;
+  let secondIndex = null;
+  let count = 0;
+  for (let i = 0; i < tree.length; i++) {
+    let current = tree[i];
+    if (frontNum === null) {
+      frontNum = current;
+      count++;
+    } else if (secondNum === null) {
+      if (current === frontNum) {
+        count++;
+      } else {
+        secondNum = current;
+        secondIndex = i - 1;
+        count++;
+      }
+    } else {
+      if (current === frontNum) {
+        count++;
+      } else if (current !== secondNum) {
+        result = Math.max(result, count);
+        i = secondIndex;
+        frontNum = null;
+        secondNum = null;
+        secondIndex = null;
+        count = 0;
+      } else {
+        count++;
+      }
+    }
+  }
+  return Math.max(result, count);
+}
