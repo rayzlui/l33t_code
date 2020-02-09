26fa330ae2c2e9fcf962790ac379c7a9d93763c6
Ray <rayzlui@gmail.com>
2020-02-08 21:30:00 -0800

    faster

--- replaceElements.js
+++ replaceElements.js
@@ -14,3 +14,24 @@ var replaceElements = function(arr) {
   });
 };
 
+//faster
+
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+function replaceElementsII(arr) {
+  let holder = 0;
+  return arr.map((x, index) => {
+    if (index === arr.length - 1) {
+      return -1;
+    }
+    if (holder > index) {
+      return arr[holder];
+    }
+    let rightSide = arr.slice(index + 1);
+    let max = Math.max(...rightSide);
+    holder = arr.indexOf(max);
+    return max;
+  });
+};
