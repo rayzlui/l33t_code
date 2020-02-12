55fe889182b6f10ee76d28b6c92e7a6596df0350
Ray <rayzlui@gmail.com>
2020-02-11 21:33:15 -0800

    better memory usage

--- deepestLeavesSum.js
+++ deepestLeavesSum.js
@@ -35,3 +35,39 @@ function deepestLeavesSum(root) {
   return holder[last].reduce((acc, curr) => acc + curr);
 }
 
+//more efficient memory
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function deepestLeavesSumII(root) {
+  let deepest = 0;
+  let sum = 0;
+  function recursion(branch, depth) {
+    if (branch.left === null && branch.right === null) {
+      if (depth === deepest) {
+        sum += branch.val;
+      } else if (depth > deepest) {
+        deepest = depth;
+        sum = branch.val;
+      }
+      return;
+    }
+    if (branch.left) {
+      recursion(branch.left, depth + 1);
+    }
+    if (branch.right) {
+      recursion(branch.right, depth + 1);
+    }
+  }
+  recursion(root, 0);
+  return sum;
+}
