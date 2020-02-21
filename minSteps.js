0227fc711f3c53e1d40e54a4ea79ac10d1b4f726
Ray <rayzlui@gmail.com>
2020-02-20 21:49:13 -0800

    faster

--- minSteps.js
+++ minSteps.js
@@ -28,3 +28,40 @@ function minSteps(s, t) {
   let keys = Object.keys(holder);
   return keys.reduce((acc, curr) => holder[curr] + acc, 0);
 }
+
+//faster
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+function minStepsII(s, t) {
+  let holder = {};
+  for (let i = 0; i < s.length; i++) {
+    let x = s[i];
+    let y = t[i];
+    if (holder[x]) {
+      holder[x]++;
+      if (holder[x] === 0) {
+        delete holder[x];
+      }
+    } else {
+      holder[x] = 1;
+    }
+    if (holder[y]) {
+      holder[y]--;
+      if (holder[y] === 0) {
+        delete holder[y];
+      }
+    } else {
+      holder[y] = -1;
+    }
+  }
+  return Object.values(holder).reduce((acc, curr) => {
+    if (curr > 0) {
+      acc = acc + curr;
+    }
+    return acc;
+  }, 0);
+}
