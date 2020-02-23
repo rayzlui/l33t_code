510bb69a85ff7e7a7dbadb8af8bc04c6d8f6a101
Ray <rayzlui@gmail.com>
2020-02-22 23:04:27 -0800

    faster

--- minSetSize.js
+++ minSetSize.js
@@ -25,3 +25,29 @@ function minSetSize(arr) {
   }
 }
 
+//faster
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+function minSetSizeII(arr) {
+  let counter = arr.reduce((acc, curr) => {
+    if (acc[curr]) {
+      acc[curr]++;
+    } else {
+      acc[curr] = 1;
+    }
+    return acc;
+  }, {});
+  let values = Object.values(counter).sort((x, y) => y - x);
+  let count = 0;
+  let sum = 0;
+  for (let i = 0; i < values.length; i++) {
+    sum += values[i];
+    count++;
+    if (sum >= arr.length / 2) {
+      return count;
+    }
+  }
+}
