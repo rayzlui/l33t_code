d0b7ab70c01f591c43878227a4b55592155df923
Ray <rayzlui@gmail.com>
2020-02-27 21:47:16 -0800

    faster

--- checkIfExist.js
+++ checkIfExist.js
@@ -12,3 +12,28 @@ function checkIfExist(arr) {
   return evens.some(x => arr.includes(x / 2));
 }
 
+//faster
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+function checkIfExistII(arr) {
+  let zero = 0;
+  let seen = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      zero++;
+      if (zero === 2) {
+        return true;
+      }
+    }
+    let double = arr[i] * 2;
+    let half = arr[i] / 2;
+    if (seen[double] || seen[half]) {
+      return true;
+    }
+    seen[arr[i]] = true;
+  }
+  return false;
+}
