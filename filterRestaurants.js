cae2853b56169462eb473b22bb46921a54fe70c3
Ray <rayzlui@gmail.com>
2020-02-28 23:23:56 -0800

    faster

--- filterRestaurants.js
+++ filterRestaurants.js
@@ -22,3 +22,34 @@ function filterRestaurants(restaurants, veganFriendly, maxPrice, maxDistance) {
     })
     .map(y => y[0]);
 }
+
+//faster
+
+/**
+ * @param {number[][]} restaurants
+ * @param {number} veganFriendly
+ * @param {number} maxPrice
+ * @param {number} maxDistance
+ * @return {number[]}
+ */
+function filterRestaurantsII(
+  restaurants,
+  veganFriendly,
+  maxPrice,
+  maxDistance,
+) {
+  if (veganFriendly) {
+    restaurants = restaurants.filter(x => x[2] === 1);
+  }
+  restaurants = restaurants.filter(
+    x => x[4] <= maxDistance && x[3] <= maxPrice,
+  );
+  return restaurants
+    .sort((x, y) => {
+      if (y[1] === x[1]) {
+        return y[0] - x[0];
+      }
+      return y[1] - x[1];
+    })
+    .map(y => y[0]);
+}
