e9ef5db0f451b2ad6164e2a5744133401407bf93
Ray <rayzlui@gmail.com>
2020-02-29 22:03:32 -0800

    1361 from leetcode

---
+++ validateBrinaryTreeNode.js
@@ -0,0 +1,27 @@
+//From 1361 in LeetCode
+
+/**
+ * @param {number} n
+ * @param {number[]} leftChild
+ * @param {number[]} rightChild
+ * @return {boolean}
+ */
+function validateBinaryTreeNodes(n, leftChild, rightChild) {
+  //needs to cycle + only lands on once.
+  let seen = {};
+  for (let i = 0; i < n; i++) {
+    let left = leftChild[i];
+    let right = rightChild[i];
+    if (seen[left] || seen[right]) {
+      return false;
+    }
+    if (left !== -1) {
+      seen[left] = true;
+    }
+    if (right !== -1) {
+      seen[right] = true;
+    }
+  }
+  let keys = Object.keys(seen).length;
+  return keys === n - 1;
+}
