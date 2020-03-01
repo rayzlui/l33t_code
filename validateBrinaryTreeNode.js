875cf256900da2f8836298d97e7b357a11f5b9ae
Ray <rayzlui@gmail.com>
2020-02-29 22:03:41 -0800

    faster

--- validateBrinaryTreeNode.js
+++ validateBrinaryTreeNode.js
@@ -25,3 +25,30 @@ function validateBinaryTreeNodes(n, leftChild, rightChild) {
   let keys = Object.keys(seen).length;
   return keys === n - 1;
 }
+
+//faster
+
+/**
+ * @param {number} n
+ * @param {number[]} leftChild
+ * @param {number[]} rightChild
+ * @return {boolean}
+ */
+function validateBinaryTreeNodesII(n, leftChild, rightChild) {
+  //needs to cycle + only lands on once.
+  let seen = new Map();
+  for (let i = 0; i < n; i++) {
+    let left = leftChild[i];
+    let right = rightChild[i];
+    if (seen.get(left) || seen.get(right)) {
+      return false;
+    }
+    if (left !== -1) {
+      seen.set(left, true);
+    }
+    if (right !== -1) {
+      seen.set(right, true);
+    }
+  }
+  return seen.size === n - 1;
+}
