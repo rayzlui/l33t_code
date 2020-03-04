60894be467995bc95b2a1c0270b780b2f306dd05
Ray <rayzlui@gmail.com>
2020-03-03 22:44:29 -0800

    faster

--- smallerThanCurrent.js
+++ smallerThanCurrent.js
@@ -7,3 +7,14 @@
 function smallerNumbersThanCurrent(nums) {
   return nums.map(x => nums.filter(y => y < x).length);
 }
+
+//faster
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function smallerNumbersThanCurrentII(nums) {
+  let sortedNums = nums.slice().sort((x, y) => x - y);
+  return nums.map(x => sortedNums.indexOf(x));
+}
