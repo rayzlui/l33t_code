7663d807b1fa4c6472ef0b9a155fd855054de90c
Ray <rayzlui@gmail.com>
2020-03-03 22:46:56 -0800

    cleaner

--- canReach.js
+++ canReach.js
@@ -33,3 +33,36 @@ function canReach(arr, start) {
   }
   return false;
 }
+
+//cleaner
+
+/**
+ * @param {number[]} arr
+ * @param {number} start
+ * @return {boolean}
+ */
+function canReachII(arr, start) {
+  //recursion through two paths, ends either when we hit target or it cycles.
+  let visitedIndices = {};
+  if (arr[start] === 0) {
+    return true;
+  }
+  let queue = [start + arr[start], start - arr[start]];
+  for (let i = 0; i < queue.length; i++) {
+    let current = queue[i];
+    if (arr[current] === 0) {
+      return true;
+    }
+    let add = current + arr[current];
+    let sub = current - arr[current];
+    if (visitedIndices[add] === undefined && add < arr.length) {
+      queue.push(add);
+      visitedIndices[add] = true;
+    }
+    if (visitedIndices[sub] === undefined && sub < arr.length) {
+      queue.push(sub);
+      visitedIndices[sub] = true;
+    }
+  }
+  return false;
+}
