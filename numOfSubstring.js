ea649a997145acc20cccb57137bc834096104e96
Ray <rayzlui@gmail.com>
2020-03-03 22:45:38 -0800

    faster

--- numOfSubstring.js
+++ numOfSubstring.js
@@ -29,3 +29,52 @@ function numberOfSubstrings(s) {
   }
   return result;
 }
+
+//faster
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function numberOfSubstringsII(s) {
+  let result = 0;
+  let countA = 0;
+  let countB = 0;
+  let countC = 0;
+  let y = 0;
+  let x = 0;
+  for (; x < s.length - 2; ) {
+    if (countA > 0 && countB > 0 && countC > 0) {
+      result += s.length - (y - 1);
+      let old = s[x];
+
+      if (old === 'a') {
+        countA--;
+      }
+      if (old === 'b') {
+        countB--;
+      }
+      if (old === 'c') {
+        countC--;
+      }
+      x++;
+    } else {
+      let current = s[y];
+      if (current === 'a') {
+        countA++;
+      }
+      if (current === 'b') {
+        countB++;
+      }
+      if (current === 'c') {
+        countC++;
+      }
+      if (y === s.length) {
+        x++;
+      } else {
+        y++;
+      }
+    }
+  }
+  return result;
+}
