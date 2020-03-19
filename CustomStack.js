f92df5fb3781f9c92cf3a768e00a84680d727c74
Ray <rayzlui@gmail.com>
2020-03-18 21:51:17 -0700

    faster

--- CustomStack.js
+++ CustomStack.js
@@ -54,3 +54,54 @@ CustomStack.prototype.increment = function(k, val) {
  */
 
 //faster
+
+/**
+ * @param {number} maxSize
+ */
+class CustomStackII {
+  constructor(maxSize) {
+    this.max = maxSize;
+    this.stack = [];
+  }
+}
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+CustomStack.prototype.push = function(x) {
+  if (this.stack.length < this.max) {
+    this.stack.push(x);
+  }
+};
+
+/**
+ * @return {number}
+ */
+CustomStack.prototype.pop = function() {
+  if (this.stack.length > 0) {
+    let val = this.stack.pop();
+    return val;
+  }
+  return -1;
+};
+
+/**
+ * @param {number} k
+ * @param {number} val
+ * @return {void}
+ */
+CustomStack.prototype.increment = function(k, val) {
+  let min = k < this.stack.length ? k : this.stack.length;
+  for (let i = 0; i < min; i++) {
+    this.stack[i] += val;
+  }
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * var obj = new CustomStack(maxSize)
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * obj.increment(k,val)
+ */
