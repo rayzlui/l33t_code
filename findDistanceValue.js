b28918abab2424aeb1356a31748b0454ed0d5a0d
Ray <rayzlui@gmail.com>
2020-03-24 21:07:55 -0700

    faster

--- findDistanceValue.js
+++ findDistanceValue.js
@@ -11,3 +11,28 @@ function findTheDistanceValue(arr1, arr2, d) {
     return arr2.map(y => Math.abs(y - x)).filter(z => z <= d).length === 0;
   }).length;
 };
+
+//faster
+
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @param {number} d
+ * @return {number}
+ */
+function findTheDistanceValueII(arr1, arr2, d) {
+  let count = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    let bad = false;
+    for (let j = 0; j < arr2.length; j++) {
+      if (Math.abs(arr1[i] - arr2[j]) <= d) {
+        bad = true;
+        break;
+      }
+    }
+    if (!bad) {
+      count++;
+    }
+  }
+  return count;
+};
