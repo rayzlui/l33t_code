ceb15731cc9e1461f814a2309f118cac499254c1
Ray <rayzlui@gmail.com>
2020-04-02 21:15:27 -0700

    faster

--- mergeKList.js
+++ mergeKList.js
@@ -52,3 +52,57 @@ function mergeKLists(lists) {
   }
   return start;
 }
+
+//faster
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+function mergeKListsII(lists) {
+  let holder = lists.reduce((acc, curr, index) => {
+    if (curr) {
+      if (acc[curr.val]) {
+        acc[curr.val].push(curr);
+      } else {
+        acc[curr.val] = [curr];
+      }
+    }
+    return acc;
+  }, {});
+  let newList = null;
+  let start = null;
+  let keys = Object.keys(holder);
+  while (Object.keys(holder).length !== 0) {
+    let lowest = Math.min(...keys);
+    if (newList) {
+      newList.next = holder[lowest][0];
+      newList = newList.next;
+    } else {
+      newList = holder[lowest][0];
+      start = newList;
+    }
+
+    let first = holder[lowest].shift();
+    if (first.next !== null) {
+      if (holder[first.next.val]) {
+        holder[first.next.val].push(first.next);
+      } else {
+        holder[first.next.val] = [first.next];
+      }
+    }
+    if (holder[lowest].length === 0) {
+      delete holder[lowest];
+    }
+
+    keys = Object.keys(holder);
+  }
+  return start;
+}
