77c47e0ee7e9e0a2c3e7ea5662e81e595ff9ee3d
Ray <rayzlui@gmail.com>
2020-04-13 21:44:31 -0700

    less memory

--- jumpGameII.js
+++ jumpGameII.js
@@ -24,3 +24,30 @@ function jump(nums) {
   }
   return jumps;
 }
+
+//less memory
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function jumpII(nums) {
+  let start = 0;
+  let jumps = 0;
+  while (start < nums.length - 1) {
+    jumps++;
+    let moves = nums[start];
+    if (moves + start >= nums.length - 1) {
+      break;
+    }
+    let index = 0;
+    for (let i = 0; i < moves; i++) {
+      let curr = nums[i + start + 1];
+      if (curr + i >= nums[index + start + 1] + index) {
+        index = i;
+      }
+    }
+    start += index + 1;
+  }
+  return jumps;
+}
