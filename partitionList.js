2dca26e836687a9f5e3df3e3c0bcf1957736516a
Ray <rayzlui@gmail.com>
2020-04-15 21:55:48 -0700

    faster

--- partitionList.js
+++ partitionList.js
@@ -55,3 +55,58 @@ function partition(head, x) {
   lowerList.next = higher[0];
   return lower[0];
 }
+
+//faster
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+function partitionII(head, x) {
+  if (head === null) {
+    return null;
+  }
+  let current = head;
+  let array = [];
+  let newHead;
+  let topHigh;
+  while (current) {
+    array.push(current);
+    current = current.next;
+  }
+  let lower;
+  let higher;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].val >= x) {
+      if (higher !== undefined) {
+        higher.next = array[i];
+        higher = higher.next;
+      } else {
+        higher = array[i];
+        topHigh = array[i];
+      }
+    } else {
+      if (lower !== undefined) {
+        lower.next = array[i];
+        lower = lower.next;
+      } else {
+        lower = array[i];
+        newHead = array[i];
+      }
+    }
+  }
+  if (lower && higher) {
+    higher.next = null;
+    lower.next = topHigh;
+    return newHead;
+  }
+  return head;
+}
