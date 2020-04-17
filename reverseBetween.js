19e35e5c029e49005515ef0328271be02b23e16c
Ray <rayzlui@gmail.com>
2020-04-16 21:45:52 -0700

    faster

--- reverseBetween.js
+++ reverseBetween.js
@@ -55,3 +55,61 @@ function reverseBetween(head, m, n) {
   }
   return head;
 }
+
+//faster
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} m
+ * @param {number} n
+ * @return {ListNode}
+ */
+function reverseBetweenII(head, m, n) {
+  let preM = null;
+  let postN = null;
+  let current = head;
+  let i = 1;
+  let reverse = null;
+  let last = null;
+  while (current) {
+    if (i === m - 1) {
+      preM = current;
+    }
+    if (i === n + 1) {
+      postN = current;
+    }
+    let pre = current;
+    current = current.next;
+    if (i >= m && i <= n) {
+      if (reverse) {
+        pre.next = reverse;
+        reverse = pre;
+      } else {
+        reverse = pre;
+        last = pre;
+      }
+    }
+    i++;
+  }
+  last.next = null;
+  if (preM && postN) {
+    last.next = postN;
+    preM.next = reverse;
+    return head;
+  } else if (preM) {
+    preM.next = reverse;
+    return head;
+  } else if (postN) {
+    last.next = postN;
+    return reverse;
+  } else {
+    return reverse;
+  }
+}
