d20b00e2e56d3d92dc13262aedacd28c10c695a6
Ray <rayzlui@gmail.com>
2020-04-17 20:56:07 -0700

    faster

--- connectBST.js
+++ connectBST.js
@@ -45,3 +45,52 @@ function connect(root) {
     }
   }
 }
+
+//faster
+
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+function connectII(root) {
+  //bfs is arbitrary
+  //store in a stack that gets returned because it's a perfect binary. eg go down right and set null, then to left and set null and store the left in the stack that will receive it when we go down left side.
+  if (root === null) {
+    return root;
+  }
+  let queue = [root, 'end'];
+  let next = null;
+  for (let i = 0; i < queue.length; i++) {
+    let current = queue[i];
+    if (current === 'end') {
+      next.next = null;
+      queue.push('end');
+      next = null;
+    } else {
+      if (current.left) {
+        if (next) {
+          next.next = current.left;
+          next = next.next;
+          next.next = current.right;
+          next = next.next;
+        } else {
+          next = current.left;
+          next.next = current.right;
+          next = next.next;
+        }
+        queue.push(current.left, current.right);
+      } else {
+        return root;
+      }
+    }
+  }
+}
