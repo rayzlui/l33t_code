0004b0667d0c6650df08583ac5eaad8e94a1f1f2
Ray <rayzlui@gmail.com>
2020-04-22 21:49:56 -0700

    faster

--- rotateRight.js
+++ rotateRight.js
@@ -40,3 +40,45 @@ function rotateRight(head, k) {
   }, newHead);
   return newHead;
 }
+
+//faster
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+function rotateRightII(head, k) {
+  if (head === null) {
+    return null;
+  }
+  if (k === 0) {
+    return head;
+  }
+  let values = {};
+  let current = head;
+  let i = 1;
+  while (current) {
+    values[i] = current;
+    current = current.next;
+    i++;
+  }
+  let keys = Object.keys(values);
+  if (keys.length === 1) {
+    return head;
+  }
+  let shift = keys.length - (k % keys.length);
+  if (k % keys.length === 0) {
+    return head;
+  }
+  values[shift].next = null;
+  values[keys[keys.length - 1]].next = values[1];
+  return values[shift + 1];
+}
