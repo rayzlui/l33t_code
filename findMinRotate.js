1c355c782d7716337da2bead072aa813acceab54
Ray <rayzlui@gmail.com>
2020-04-24 21:32:13 -0700

    faster

--- findMinRotate.js
+++ findMinRotate.js
@@ -7,3 +7,29 @@
 function findMin(nums) {
   return Math.min(...nums);
 }
+
+//faster
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function findMinII(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] < nums[right]) {
+      right = mid;
+    } else if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      if (nums[right] === nums[right - 1]) {
+        right--;
+      } else {
+        left = mid + 1;
+      }
+    }
+  }
+  return nums[left];
+}
