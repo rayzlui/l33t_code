66329f049c3de0b5c2a1dbb14799cd6fd09b64f0
Ray <rayzlui@gmail.com>
2020-04-30 21:35:47 -0700

    faster

--- maxScoreCount.js
+++ maxScoreCount.js
@@ -17,3 +17,25 @@ function maxScore(s) {
   }
   return max;
 }
+//faster
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function maxScoreII(s) {
+  s = s.split('');
+  let front = s.slice(0, 1).filter(x => x === '0').length;
+  let back = s.slice(1).filter(x => x === '1').length;
+  let max = front + back;
+  for (let i = 1; i < s.length - 1; i++) {
+    let current = s[i];
+    if (current === '0') {
+      front++;
+    } else {
+      back--;
+    }
+    max = Math.max(front + back, max);
+  }
+  return max;
+}
