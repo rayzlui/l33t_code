be8d286ba27fca7caf45fe08c1b158a43be2befe
Ray <rayzlui@gmail.com>
2020-05-04 21:35:21 -0700

    faster

--- countLargestGroup.js
+++ countLargestGroup.js
@@ -26,3 +26,34 @@ function countLargestGroup(n) {
   let groups = holder.filter(x => x.length === max).length;
   return groups;
 }
+
+//faster
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+function countLargestGroupII(n) {
+  let holder = [];
+  let size = {};
+  for (let i = 1; i <= n; i++) {
+    let sum = i
+      .toString()
+      .split('')
+      .reduce((acc, curr) => {
+        acc = acc + curr / 1;
+        return acc;
+      }, 0);
+    if (holder[sum]) {
+      holder[sum].push(i);
+    } else {
+      holder[sum] = [i];
+    }
+    if (size[holder[sum].length]) {
+      size[holder[sum].length]++;
+    } else {
+      size[holder[sum].length] = 1;
+    }
+  }
+  return size[Math.max(...Object.keys(size))];
+}
