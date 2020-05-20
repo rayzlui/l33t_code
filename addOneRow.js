b5cfa53c9eac4c2ab57236df630d0769beea49ff
Ray <rayzlui@gmail.com>
2020-05-19 21:58:19 -0700

    623 from leetcode

---
+++ addOneRow.js
@@ -0,0 +1,55 @@
+//From 623 in LeetCode
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} v
+ * @param {number} d
+ * @return {TreeNode}
+ */
+function addOneRow(root, v, d) {
+  if (d === 1) {
+    let newHead = new TreeNode(v);
+    newHead.left = root;
+    return newHead;
+  }
+
+  let count = 1;
+  let queue = [root, 'end'];
+  let parents = [root];
+  let offspring = [];
+  let i = 0;
+  while (count !== d - 1) {
+    let current = queue[i];
+
+    if (current === 'end') {
+      count++;
+      queue.push('end');
+      parents = offspring;
+      offspring = [];
+    } else {
+      if (current.left) {
+        queue.push(current.left);
+        offspring.push(current.left);
+      }
+      if (current.right) {
+        queue.push(current.right);
+        offspring.push(current.right);
+      }
+    }
+    i++;
+  }
+  parents.forEach(x => {
+    let newLeft = new TreeNode(v);
+    let newRight = new TreeNode(v);
+    newLeft.left = x.left;
+    newRight.right = x.right;
+    x.left = newLeft;
+    x.right = newRight;
+  });
