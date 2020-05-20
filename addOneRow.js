b5ad6f5101419c18d3503ff39c791c9235ff017b
Ray <rayzlui@gmail.com>
2020-05-19 21:58:31 -0700

    faster

--- addOneRow.js
+++ addOneRow.js
@@ -53,3 +53,62 @@ function addOneRow(root, v, d) {
     x.left = newLeft;
     x.right = newRight;
   });
+
+  //faster
+
+  /**
+   * Definition for a binary tree node.
+   * function TreeNode(val) {
+   *     this.val = val;
+   *     this.left = this.right = null;
+   * }
+   */
+  /**
+   * @param {TreeNode} root
+   * @param {number} v
+   * @param {number} d
+   * @return {TreeNode}
+   */
+  function addOneRow(root, v, d) {
+    if (d === 1) {
+      let newHead = new TreeNode(v);
+      newHead.left = root;
+      return newHead;
+    }
+
+    let count = 1;
+    let parents = [root];
+    let offspring = [];
+    let i = 0;
+    while (count !== d - 1) {
+      let current = parents[i];
+
+      if (i === parents.length) {
+        count++;
+        parents = offspring;
+        offspring = [];
+        i = 0;
+      } else {
+        i++;
+        if (current.left) {
+          offspring.push(current.left);
+        }
+        if (current.right) {
+          offspring.push(current.right);
+        }
+      }
+    }
+    parents.forEach(x => {
+      let newLeft = new TreeNode(v);
+      let newRight = new TreeNode(v);
+      newLeft.left = x.left;
+      newRight.right = x.right;
+      x.left = newLeft;
+      x.right = newRight;
+    });
+
+    return root;
+  }
+
+  return root;
+}
