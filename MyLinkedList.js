64560e9f3d3de9354b843f3c7c52a2bb4c3fe9c9
Ray <rayzlui@gmail.com>
2020-05-21 21:41:01 -0700

    faster

--- MyLinkedList.js
+++ MyLinkedList.js
@@ -116,3 +116,74 @@ MyLinkedList.prototype.deleteAtIndex = function(index) {
  * obj.addAtIndex(index,val)
  * obj.deleteAtIndex(index)
  */
+
+//faster
+
+/**
+ * Initialize your data structure here.
+ */
+class MyLinkedListII {
+  constructor() {
+    this.head = [];
+  }
+}
+
+/**
+ * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+ * @param {number} index
+ * @return {number}
+ */
+MyLinkedList.prototype.get = function(index) {
+  return this.head[index] !== undefined ? this.head[index] : -1;
+};
+
+/**
+ * Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtHead = function(val) {
+  this.head.unshift(val);
+};
+
+/**
+ * Append a node of value val to the last element of the linked list.
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtTail = function(val) {
+  this.head.push(val);
+};
+
+/**
+ * Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+ * @param {number} index
+ * @param {number} val
+ * @return {void}
+ */
+MyLinkedList.prototype.addAtIndex = function(index, val) {
+  if (index <= this.head.length) {
+    this.head.splice(index, 0, val);
+  }
+};
+
+/**
+ * Delete the index-th node in the linked list, if the index is valid.
+ * @param {number} index
+ * @return {void}
+ */
+MyLinkedList.prototype.deleteAtIndex = function(index) {
+  if (index < this.head.length) {
+    this.head.splice(index, 1);
+  }
+};
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
