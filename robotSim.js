8136493883202d0b9c4271fd5ac33af64f64d464
Ray <rayzlui@gmail.com>
2020-06-02 21:30:41 -0700

    faster

--- robotSim.js
+++ robotSim.js
@@ -68,3 +68,74 @@ function robotSim(commands, obstacles) {
   }
   return max;
 }
+
+//faster
+
+/**
+ * @param {number[]} commands
+ * @param {number[][]} obstacles
+ * @return {number}
+ */
+function robotSimII(commands, obstacles) {
+  let location = [0, 0];
+  let betterObs =
+    obstacles.length > 0
+      ? obstacles.reduce((acc, curr) => {
+          let string = curr.join(',');
+          acc[string] = true;
+          return acc;
+        }, {})
+      : {};
+  let max = 0;
+  let direction = 0;
+  for (let i = 0; i < commands.length; i++) {
+    let move = commands[i];
+    if (move === -2) {
+      direction = direction ? direction - 90 : 270;
+    } else if (move === -1) {
+      direction = direction === 270 ? 0 : direction + 90;
+    } else {
+      switch (direction) {
+        case 0:
+          for (let j = 0; j < move; j++) {
+            location[1] = location[1] + 1;
+            if (betterObs[`${location[0]},${location[1]}`]) {
+              location[1] = location[1] - 1;
+              break;
+            }
+          }
+          break;
+        case 90:
+          for (let j = 0; j < move; j++) {
+            location[0] = location[0] + 1;
+            if (betterObs[`${location[0]},${location[1]}`]) {
+              location[0] = location[0] - 1;
+              break;
+            }
+          }
+          break;
+        case 180:
+          for (let j = 0; j < move; j++) {
+            location[1] = location[1] - 1;
+
+            if (betterObs[`${location[0]},${location[1]}`]) {
+              location[1] = location[1] + 1;
+              break;
+            }
+          }
+          break;
+        default:
+          for (let j = 0; j < move; j++) {
+            location[0] = location[0] - 1;
+            if (betterObs[`${location[0]},${location[1]}`]) {
+              location[0] = location[0] + 1;
+              break;
+            }
+          }
+          break;
+      }
+    }
+    max = Math.max(location[0] ** 2 + location[1] ** 2, max);
+  }
+  return max;
+}
