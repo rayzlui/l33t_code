b8d13529a4a850700ade335e7ec15d568708a6bc
Ray <rayzlui@gmail.com>
2020-06-18 21:14:29 -0700

    fasterer

--- findLeastNumInt.js
+++ findLeastNumInt.js
@@ -48,3 +48,31 @@ function findLeastNumOfUniqueIntsII(arr, k) {
   });
   return new Set(arr.slice(k)).size;
 }
+
+//fasterer
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+function findLeastNumOfUniqueIntsIII(arr, k) {
+  let holder = arr.reduce((acc, curr) => {
+    if (acc[curr]) {
+      acc[curr]++;
+    } else {
+      acc[curr] = 1;
+    }
+    return acc;
+  }, {});
+  let values = Object.values(holder).sort((x, y) => x - y);
+  let i = 0;
+  for (; i < values.length; i++) {
+    if (k >= values[i]) {
+      k -= values[i];
+    } else {
+      break;
+    }
+  }
+  return values.length - i;
+}
