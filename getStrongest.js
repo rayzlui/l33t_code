57763c33705d05b1a977d98fb83575a6b499179c
Ray <rayzlui@gmail.com>
2020-06-19 21:35:07 -0700

    1471 from leetcode

---
+++ getStrongest.js
@@ -0,0 +1,28 @@
+//From 1471 in LeetCode
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number[]}
+ */
+
+function getStrongest(arr, k) {
+  arr.sort((x, y) => x - y);
+  let median =
+    arr.length % 2 === 0
+      ? arr[Math.floor(arr.length) / 2 - 1]
+      : arr[Math.floor(arr.length / 2)];
+  let diffs = arr.reduce((acc, curr, index) => {
+    acc[index] = Math.abs(curr - median);
+    return acc;
+  }, {});
+  let keys = Object.keys(diffs);
+  keys.sort((x, y) => {
+    if (diffs[y] === diffs[x]) {
+      return arr[y] - arr[x];
+    } else {
+      return diffs[y] - diffs[x];
+    }
+  });
+  return keys.slice(0, k).map(x => arr[x]);
+}
