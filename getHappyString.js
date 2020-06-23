d777f539dd69a8a7572d3dae8d652d6fa51b626e
Ray <rayzlui@gmail.com>
2020-06-22 20:00:20 -0700

    faster

--- getHappyString.js
+++ getHappyString.js
@@ -22,3 +22,30 @@ function getHappyString(n, k) {
   recursion('');
   return words[k - 1] ? words[k - 1] : '';
 }
+
+//faster
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {string}
+ */
+function getHappyStringII(n, k) {
+  let words = [];
+  let letters = ['a', 'b', 'c'];
+  function recursion(word) {
+    if (word.length === n) {
+      words.push(word);
+      return words.length === k ? true : false;
+    }
+    for (let i = 0; i < letters.length; i++) {
+      if (word[word.length - 1] !== letters[i]) {
+        let shouldBreak = recursion(word + letters[i]);
+        if (shouldBreak) {
+          return true;
+        }
+      }
+    }
+  }
+  recursion('');
+  return words[k - 1] ? words[k - 1] : '';
+}
