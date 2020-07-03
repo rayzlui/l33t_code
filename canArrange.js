73749188bff73c04500022e07b5331668f37c226
Ray <rayzlui@gmail.com>
2020-07-02 21:27:21 -0700

    faster

--- canArrange.js
+++ canArrange.js
@@ -9,3 +9,40 @@ function canArrange(arr, k) {
   let sum = arr.reduce((acc, curr) => acc + curr);
   return sum % k === 0 && sum !== k;
 }
+
+//correct
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {boolean}
+ */
+function canArrangeII(arr, k) {
+  let seen = {};
+  for (let i = 0; i < arr.length; i++) {
+    let remainder = arr[i] % k;
+    if (remainder < 0) {
+      remainder = k + remainder;
+    }
+    if (seen[remainder]) {
+      seen[remainder]++;
+    } else {
+      seen[remainder] = 1;
+    }
+  }
+  if (seen[0] % 2 === 1) {
+    return false;
+  }
+  delete seen[0];
+  let keys = Object.keys(seen);
+
+  for (let i = 0; i < keys.length; i++) {
+    let x = keys[i] / 1;
+    let remainder = k - x;
+
+    if (seen[remainder] !== seen[x]) {
+      return false;
+    }
+  }
+  return true;
+}
