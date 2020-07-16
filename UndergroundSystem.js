3141be546ff2e31d00454c6e5acf3831ddf83e34
Ray <rayzlui@gmail.com>
2020-07-15 21:37:01 -0700

    faster

--- UndergroundSystem.js
+++ UndergroundSystem.js
@@ -54,3 +54,61 @@ UndergroundSystem.prototype.getAverageTime = function(
  * obj.checkOut(id,stationName,t)
  * var param_3 = obj.getAverageTime(startStation,endStation)
  */
+
+//less memory
+
+class UndergroundSystemII {
+  constructor() {
+    this.riders = {};
+    this.times = {};
+  }
+}
+
+/**
+ * @param {number} id
+ * @param {string} stationName
+ * @param {number} t
+ * @return {void}
+ */
+UndergroundSystem.prototype.checkIn = function(id, stationName, t) {
+  this.riders[id] = [stationName, t];
+};
+
+/**
+ * @param {number} id
+ * @param {string} stationName
+ * @param {number} t
+ * @return {void}
+ */
+UndergroundSystem.prototype.checkOut = function(id, stationName, t) {
+  let checkIn = this.riders[id];
+  let startStation = checkIn[0];
+  let startTime = checkIn[1];
+  if (this.times[`${startStation}${stationName}`]) {
+    this.times[`${startStation}${stationName}`][0] += t - startTime;
+    this.times[`${startStation}${stationName}`][1]++;
+  } else {
+    this.times[`${startStation}${stationName}`] = [t - startTime, 1];
+  }
+};
+
+/**
+ * @param {string} startStation
+ * @param {string} endStation
+ * @return {number}
+ */
+UndergroundSystem.prototype.getAverageTime = function(
+  startStation,
+  endStation,
+) {
+  let times = this.times[`${startStation}${endStation}`];
+  return times[0] / times[1];
+};
+
+/**
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * var obj = new UndergroundSystem()
+ * obj.checkIn(id,stationName,t)
+ * obj.checkOut(id,stationName,t)
+ * var param_3 = obj.getAverageTime(startStation,endStation)
+ */
