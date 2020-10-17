26130dfb38ea38977971d5e5a26aca97c873637c
Ray <rayzlui@gmail.com>
2020-10-16 21:36:42 -0700

    1546 from leetcode

---
+++ maxNonOverlap.js
@@ -0,0 +1,31 @@
+//From 1546 in LeetCode
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+function maxNonOverlapping(nums, target) {
+  let presum = { 0: -1 };
+  let indices = [];
+  let sum = 0;
+  nums.forEach((x, index) => {
+    sum += x;
+    if (presum[sum - target] !== undefined) {
+      indices.push([presum[sum - target] + 1, index]);
+    }
+    presum[sum] = index;
+  });
+  let result = 0;
+  let last = -1;
+  for (let i = 0; i < indices.length; i++) {
+    let current = indices[i];
+    let start = current[0];
+    let end = current[1];
+    if (start > last) {
+      result++;
+      last = end;
+    }
+  }
+  return result;
+}
