da0b7a411e4984570f2b770e52ec6745233febc5
Ray <rayzlui@gmail.com>
2020-11-23 21:54:32 -0800

    1662 from leetcode

---
+++ arrayStringsEqual.js
@@ -0,0 +1,10 @@
+//From 1662 in LeetCode
+
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+function arrayStringsAreEqual(word1, word2) {
+  return word1.join('') === word2.join('');
+}
