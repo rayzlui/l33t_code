8bc11a4351f49454a709be644a877826f4e99100
Ray <rayzlui@gmail.com>
2021-02-04 21:26:30 -0800

    faster

--- maxWealth.js
+++ maxWealth.js
@@ -7,3 +7,17 @@
 function maximumWealth(accounts) {
   return Math.max(...accounts.map(x => x.reduce((acc, curr) => acc + curr)));
 }
+
+//faster
+
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+function maximumWealthII(accounts) {
+  let max = 0;
+  accounts.forEach(x => {
+    max = Math.max(max, x.reduce((acc, curr) => acc + curr));
+  });
+  return max;
+}
