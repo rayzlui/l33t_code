e6651840f82c60b5fd4799ab6b7cbdf156fdadf4
Ray <rayzlui@gmail.com>
2021-04-09 20:36:43 -0700

    1577 from leetcode

--- numTriplets.js
+++ numTriplets.js
@@ -43,3 +43,47 @@ function numTriplets(nums1, nums2) {
   });
   return result;
 }
+
+//faster
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+function numTripletsII(nums1, nums2) {
+  let nums1Products = {};
+  let nums2Products = {};
+  let copy1 = nums1.slice(1);
+  let copy2 = nums2.slice(1);
+  let result = 0;
+  nums1.forEach(x => {
+    copy1.forEach(y => {
+      let product = x * y;
+      if (nums1Products[product]) {
+        nums1Products[product]++;
+      } else {
+        nums1Products[product] = 1;
+      }
+    });
+    if (nums2Products[x * x]) {
+      nums2Products[x * x]++;
+    } else {
+      nums2Products[x * x] = 1;
+    }
+    copy1.shift();
+  });
+  nums2.forEach(x => {
+    copy2.forEach(y => {
+      let product = x * y;
+      if (nums2Products[product]) {
+        result += nums2Products[product];
+      }
+    });
+    copy2.shift();
+    if (nums1Products[x * x]) {
+      result += nums1Products[x * x];
+    }
+  });
+  return result;
+}
