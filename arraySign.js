f9b80022d2b4762cb8066900de7e453d5a9398d0
Ray <rayzlui@gmail.com>
2021-04-19 21:08:58 -0700

    faster

--- arraySign.js
+++ arraySign.js
@@ -17,3 +17,23 @@ function arraySign(nums) {
   }
   return result;
 }
+
+//faster
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function arraySignII(nums) {
+  let negatives = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let current = nums[i];
+    if (current === 0) {
+      return 0;
+    }
+    if (current < 0) {
+      negatives++;
+    }
+  }
+  return negatives % 2 ? -1 : 1;
+}
